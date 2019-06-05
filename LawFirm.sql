a763bb03fb3fceb1ad2fcd41f8df5ac476b7debe
Damir Cavajda <dcavajda@gmail.com>
2019-06-05 16:16:08 +0200

    first update ER

--- LawFirm.sql
+++ LawFirm.sql
@@ -1,57 +1,42 @@
-# sipanje na server
-/*
-c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "c:\odvjetnik.sql"
-*/
-
-drop database if exists odvjetnik_ured;
-create database odvjetnik_ured default character set utf8;
-use odvjetnik_ured;
-
-create table osoba (
-osoba_sifra int not null primary key auto_increment,
-OIB int (11) not null,
-ime varchar (50) not null,                                                                                                                                               
-prezime varchar (50) not null,
-ulica varchar (50) not null,
-kucni_broj varchar (10) not null,
-grad varchar (50) not null,
-postanski_broj varchar (10) not null,
-IBAN varchar (32) not null
+drop database if exists LawFirm;
+create database LawFirm default character set utf8;
+use LawFirm;
+create table law_case(
+law_case_id int not null primary key auto_increment,
+lawyer_case_id int not null,
+client_case_id int not null,
+legal_trainee_case_id int not null
 );
-create table odvjetnik(
-odvjetnik_sifra int not null primary key auto_increment,
-osoba_sifra int not null
+create table lawyer(
+lawyer_id int not null primary key auto_increment,
+law_case int not null,
+legal_trainee int not null,
+lawyer_partner int not null,
+firstname varchar (50),
+lastname varchar(50),
+field_of_law varchar (100)
 );
-create table klijent (
-klijent_sifra int not null primary key auto_increment,
-osoba_sifra int not null
+create table client (
+client_id int not null primary key auto_increment,
+lawyer_id int not null,
+firstname varchar (50) not null,                                                                                                                                               
+lastname varchar (50) not null
 );
-create table suradnik (
-suradnik_sifra int not null primary key auto_increment,
-osoba_sifra int not null
+create table legal_trainee (
+legal_trainee_id int not null primary key auto_increment,
+firstname varchar (50),
+lastname varchar (50)
 );
-create table obrana (
-obrana_sifra int not null primary key auto_increment,
-odvjetnik_osoba_sifra int not null,
-klijent_osoba_sifra int not null
+create table partner (
+partner_id int not null primary key auto_increment
 );
-create table suradnik_obrana (
-suradnik_osoba int not NULL,
-obrana_sifra int not null
-);
-
-alter table odvjetnik add foreign key (odvjetnik_sifra) references osoba (osoba_sifra);
-alter table klijent add foreign key (klijent_sifra) references osoba (osoba_sifra);
-alter table suradnik add foreign key (suradnik_sifra) references osoba (osoba_sifra);
-ALTER TABLE obrana ADD FOREIGN KEY (odvjetnik_osoba_sifra) REFERENCES odvjetnik (odvjetnik_sifra);
-ALTER TABLE obrana ADD FOREIGN KEY (klijent_osoba_sifra) REFERENCES klijent (klijent_sifra);
-ALTER TABLE suradnik_obrana ADD FOREIGN KEY (suradnik_osoba) REFERENCES suradnik (suradnik_sifra);
-ALTER TABLE suradnik_obrana ADD FOREIGN KEY (obrana_sifra) REFERENCES obrana (obrana_sifra);
+alter table law_case add foreign key (lawyer_case_id) references lawyer (lawyer_id);
+alter table law_case add foreign key (client_case_id) references client (client_id);
+alter table lawyer add foreign key (lawyer_partner) references partner (partner_id);
+alter table lawyer add foreign key (legal_trainee) references legal_trainee (legal_trainee_id);
+alter table client add foreign key (client_id) references lawyer (lawyer_id);
 
 
-INSERT INTO odvjetnik_ured.osoba
-(`OIB`, ime, prezime, ulica, kucni_broj, grad, postanski_broj, `IBAN`) VALUES
-(12345678901, 'Èedo', 'Prodanoviæ', 'Ilica', '259', 'Zagreb', '10000', 'HR12345678912345'),
-(1236547891, 'Jadranka', 'Slokoviæ', 'Ilica', '259','Zagreb', '10000', 'HR12345698712321');
+
 
 
