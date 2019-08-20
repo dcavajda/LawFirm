a118fc9548ab830ea1a6e5741e90596e13fdee38
Damir Cavajda <dcavajda@gmail.com>
2019-08-20 12:35:08 +0200

    veza na privatno radi

--- baza.php
+++ baza.php
@@ -1,27 +1,8 @@
 <?php
 
-switch ($_SERVER["REMOTE_ADDR"]) {
-    case '127.0.0.1': //moj lokalni razvoj
-        $server="localhost";
-        $baza="lawfirm";
-        $korisnik="edunova";
-        $lozinka="edunova";
-        break;
-    
-    case '31.147.185.242': //edunova shared hosting
-        $server="localhost";
-        $baza="morfej_lawfirm";
-        $korisnik="morfej_lawfirm";
-        $lozinka="lawfirm";
-        break;
-}
+$veza = new PDO("mysql:dbname=morfej_lawfirm;host=localhost",
+"morfej_dcavajda","dcavajda",[PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"]);
 
-//echo "<pre>";
-//print_r($_SERVER);
-//echo "</pre>";
-
-//$veza = new PDO("mysql:dbname=edunovapp19;host=localhost",
+//lokalno
+//$veza = new PDO("mysql:dbname=lawfirm;host=localhost",
 //"edunova","edunova");
-
-$veza = new PDO("mysql:dbname=" . $baza . ";host=" . $server,
-$korisnik,$lozinka,[PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"]);
